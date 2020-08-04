c41f6ed552d492524be918f2599b77cb9048f4f6
Zishuo Li <l1277202997@gmail.com>
2020-08-04 23:53:30 +0800

    import

--- assignment/model.py
+++ assignment/model.py
@@ -1,5 +1,16 @@
+import arff, numpy as np
 import pandas as pd
-
+import numpy
+from sklearn.base import TransformerMixin
+from sklearn import tree
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn import svm, datasets
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import sys
+import warnings
 ###initial inport data and classification it 
 
 ###try linear regression to make the whole algrithem run.
