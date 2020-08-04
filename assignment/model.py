54a833f83f982e3b72d301fc0aa9f7dbcd5e4a96
Zishuo Li <l1277202997@gmail.com>
2020-08-04 23:46:22 +0800

    finish hw2, inset data for assignment

---
+++ assignment/model.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+###initial inport data and classification it 
+
+###try linear regression to make the whole algrithem run.
+###then use knn algrithem train the data remember to use cross validation to redeuce over-fit 
+
+###use test set to show the result
---
+++ hw2/test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+X = np.array([[0, 1, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+#print(X)
+print(X[:4, :])
