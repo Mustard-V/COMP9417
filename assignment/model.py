8a936fcf81397ae646ccedc3340f39b785b943f5
Zishuo Li <l1277202997@gmail.com>
2020-08-08 13:02:34 +0800

    inset data

--- assignment/model.py
+++ assignment/model.py
@@ -12,8 +12,16 @@ from sklearn.metrics import accuracy_score
 import sys
 import warnings
 ###initial inport data and classification it 
+train_set = pd.read_csv('D:\\2020T2\\comp9417\COMP9417\\assignment\\train.csv')
+test_set = pd.read_csv('D:\\2020T2\\comp9417\\COMP9417\\assignment\\test.csv')
+train_set.index = train_set['id']
+test_set.index = test_set['id']
 
+print("demension of train"+ str(train_set.shape) + "demension of test " + str(test_set.shape))
 ###try linear regression to make the whole algrithem run.
 ###then use knn algrithem train the data remember to use cross validation to redeuce over-fit 
+print(train_set.head(5))
+
+print(test_set.head(5))
 
 ###use test set to show the result
